447da7d19a9cfd5e53b5c8f412a529d76ab72a44
ameyreghu <48554984+ameyreghu@users.noreply.github.com>
2024-03-11 22:50:48 +0530

    pkg change

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,114 @@
-import 'package:flutter/material.dart';
-import 'package:desktop_webview_window/desktop_webview_window.dart';
+// import 'package:flutter/material.dart';
+// import 'package:desktop_webview_window/desktop_webview_window.dart';
 
-void main() {
-  runApp(const MyApp());
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       debugShowCheckedModeBanner: false,
+//       home: const HomePage(),
+//     );
+//   }
+// }
+
+// class HomePage extends StatefulWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   State<HomePage> createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   late Webview webview;
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Home'),
+//         actions: [
+//           IconButton(
+//               onPressed: () {
+//                 webview.close();
+//               },
+//               icon: Icon(Icons.close))
+//         ],
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//             onPressed: () async {
+//               webview = await WebviewWindow.create(
+//                   configuration: const CreateConfiguration(
+//                 openMaximized: true,
+//                 title: 'Flutter Ai',
+//               ));
+//               webview.launch(
+//                   "https://github.com/ameyreghu/desktop_wevb_view_test.git");
+//             },
+//             child: Text('Launch')),
+//       ),
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import "package:webview_universal/webview_universal.dart";
+
+void main(List<String> args) {
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class _MyAppState extends State<MyApp> {
+  WebViewController webViewController = WebViewController();
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  late Webview webview;
   @override
   void initState() {
     super.initState();
+    webViewController.init(
+      context: context,
+      setState: setState,
+      uri: Uri.parse("https://863f-116-68-78-139.ngrok-free.app"),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                webview.close();
-              },
-              icon: Icon(Icons.close))
-        ],
+        leading: FloatingActionButton(
+          onPressed: () {
+            webViewController.goBackSync();
+          },
+          child: Icon(Icons.abc),
+        ),
       ),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () async {
-              webview = await WebviewWindow.create(
-                  configuration: const CreateConfiguration(
-                openMaximized: true,
-                title: 'Flutter Ai',
-              ));
-              webview.launch(
-                  "https://github.com/ameyreghu/desktop_wevb_view_test.git");
-            },
-            child: Text('Launch')),
+      body: WebView(
+        controller: webViewController,
       ),
     );
   }
